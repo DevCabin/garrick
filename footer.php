221e34763e986d30b74081d7a0fecb3584fa31d1
George Featherstone <DevCabin@users.noreply.github.com>
2016-10-20 16:49:31 -0500

    First Commit
    It's gonna be fast.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Garrick
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'garrick' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'garrick' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'garrick' ), 'garrick', '<a href="https://growthhit.com/garrick-the-fastest-free-responsive-wordpress-theme" rel="designer">George Featherstone</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
